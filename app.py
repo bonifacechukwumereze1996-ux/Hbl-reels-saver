6aec9617d30edf2c48ef8fad67a6a78e64df0e09
bonifacechukwumereze1996-ux <bonifacechukwumereze1996@gmail.com>
2026-06-03 12:46:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ if os.path.exists(DOWNLOAD_FOLDER):
 # -----------------------------
 st.set_page_config(page_title="HBL Reels Saver (Fast Mode)", layout="centered")
 st.markdown("""
+<marquee>
+🔥 Welcome to HBL Reels Saver | Fast Downloads | Secure Processing | No Login Required 🚀
+</marquee>
+""", unsafe_allow_html=True)
 <div style="
     background: linear-gradient(135deg, #0f1117, #1f2937);
     color: white;
